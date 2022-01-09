dfbf0e2ee65d3a63d104ad220fc8c6d337f38dd9
Abyers <anthonyw.byers@gmail.com>
2022-01-09 16:22:34 -0600

    jquery code for localStorage

--- assets/js/locations.js
+++ assets/js/locations.js
@@ -5,6 +5,42 @@ var locationInputEl = document.querySelector        ("#username");
 var dailyForecastEl = document.querySelector  ("#daily-forecast");
 
 
+/* Tried to save locations that were made from username  could not get it to work*/
+var myLocations = {};
+
+var loadLocations = function() {
+    myLocations = JSON.parse(localStorage.getItem("myLocations"));
+
+    if(!myLocations){
+        myLocations = {
+            place: []
+        };
+    }
+
+    $.each(myLocations, function(list, arr){
+        arr.forEach(function(theLocation){
+            createLocation(theLocation.text, list);
+        });
+    });
+};
+
+var saveLocations = function() {
+    localStorage.setItem("myLocations", JSON.stringify(myLocations));
+};
+
+$("#user-location").click(function(){
+    var newLocation = $("#username").val();
+        if(newLocation){
+            createLocation(newLocation, "place");
+
+            myLocations.place.push({
+                place: newLocation
+            });
+            saveLocations();
+        }
+});
+
+
 /* Creates a button with api call has issue creating blank button */
 createLocation = function(event) {
     event.preventDefault();
@@ -18,12 +54,5 @@ createLocation = function(event) {
     
     repoContainerEl.appendChild(locationEl)
 }
-
-
-
-
-
-
-
 
 userLocationEl.addEventListener("submit", createLocation);
